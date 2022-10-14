79aef5901b593299e0aad2462792f8818d790561
Chaithra <99654380+Chaithra-BabuReddy@users.noreply.github.com>
2022-10-14 18:26:50 +0530

    return max,min,average when number array is present

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,14 @@
 module.exports=function getStatistics(numbers){
     
-    //implement the computation of statistics here
+    if (numbers.length > 0) {
+        let max = Math.max(...numbers)
+        let min = Math.min(...numbers);
+        let sum = 0;
+        numbers.forEach((x) => { sum = sum + x; });
+        let avg = sum / numbers.length;
+        return { "max": max, "min": min, "average":avg };
+    } else {
+        return {"max":NaN,"min":NaN,"average":NaN}
+    }
 }
 
