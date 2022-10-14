ddbf90b0d28a7f87e32981ef7357978e5018d923
Chaithra <99654380+Chaithra-BabuReddy@users.noreply.github.com>
2022-10-14 18:48:13 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,14 +1,15 @@
 module.exports=function getStatistics(numbers){
-    
+    let min_1, max_1,average_1;
     if (numbers.length > 0) {
-        let max = Math.max(...numbers)
-        let min = Math.min(...numbers);
+        max_1 = Math.max(...numbers)
+         min_1 = Math.min(...numbers);
         let sum = 0;
         numbers.forEach((x) => { sum = sum + x; });
-        let avg = sum / numbers.length;
-        return { "max": max, "min": min, "average":avg };
+        average_1 = sum / numbers.length;
+        return { min:min_1,max:max_1,average:average_1};
     } else {
-        return {"max":NaN,"min":NaN,"average":NaN}
+        min_1=NaN;max_1=NaN;average_1=NaN;
+        return {min:min_1,max:max_1,average:average_1}
     }
 }
 
